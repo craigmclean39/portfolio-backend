197bef51cfd4cbe70862c4262ce01a8cb6361aad
Craig McLean <craig.mclean39@gmail.com>
2022-01-07 12:33:41 -0800

    Connect to MongoDb
    Added a populateDb route that executes a function that parses a geoJson file
    and then populates the db with polygons, multipolygons, and coutries that
    reference the polys/multipolys

--- controllers/mapController.js
+++ controllers/mapController.js
@@ -1,5 +1,10 @@
 const traceroute = require('traceroute');
 const geoip = require('geoip-lite');
+const dataJson = require('../mapdata.json');
+const Country = require('../models/country');
+const Polygon = require('../models/polygon');
+const MultiPolygon = require('../models/multipolygon');
+const async = require('async');
 
 exports.index = (req, res, next) => {
   console.log('/map');
@@ -60,4 +65,63 @@ const generateObjectFromGeoData = (geoData, time) => {
   }
   console.log('return null');
   return null;
+};
+
+exports.populateDb = (req, res, next) => {
+  dataJson.features.forEach((f) => {
+    let geo = null;
+    if (f.geometry.type === 'Polygon') {
+      geo = new Polygon({
+        type: 'Polygon',
+        coordinates: f.geometry.coordinates,
+      });
+    } else if (f.geometry.type === 'MultiPolygon') {
+      geo = new MultiPolygon({
+        type: 'MultiPolygon',
+        coordinates: f.geometry.coordinates,
+      });
+    }
+
+    if (geo != null) {
+      async.waterfall(
+        [
+          function (callback) {
+            // console.log('waterfall 1');
+            geo.save((err, doc) => {
+              if (err) {
+                console.log('Error ' + err);
+              }
+
+              // console.log('Doc ' + doc);
+              callback(null, doc);
+            });
+          },
+          function (arg1, callback) {
+            // console.log('waterfall 2');
+            // console.log(arg1);
+
+            const country = new Country({
+              name: f.properties.sovereignt,
+              iso_a2: f.properties.iso_a2,
+              geometry: arg1._id,
+            });
+
+            country.save((err) => {
+              if (err) {
+                console.log('Error ' + err);
+              }
+
+              callback(null, 'done');
+            });
+          },
+        ],
+        function (err, results) {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+    }
+  });
+  res.end();
 };
--- routes/map.js
+++ routes/map.js
@@ -4,5 +4,6 @@ var router = express.Router();
 var map_controller = require('../controllers/mapController');
 
 router.get('/', map_controller.index);
+// router.get('/api/populateDb', map_controller.populateDb);
 
 module.exports = router;
